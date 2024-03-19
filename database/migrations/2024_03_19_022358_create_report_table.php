97b12595d1238a2416232320807c637bebffac87
kikuchihibiki <113155569+kikuchihibiki@users.noreply.github.com>
2024-03-19 11:45:06 +0900

    [add]マイグレーションファイルの作成 close #3

---
+++ database/migrations/2024_03_19_022358_create_report_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('report', function (Blueprint $table) {
+            $table->id();
+            $table->integer('genre')->unique();
+            $table->string('contents')->unique();
+            $table->integer('con_num');
+            $table->tinyInteger('delete_flag')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('report');
+    }
+};
