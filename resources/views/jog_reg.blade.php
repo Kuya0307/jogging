353cc870c645646d5715b02550379201c7034c62
F.RN1945 <r.furukawa.sys23@morijyobi.ac.jp>
2024-03-19 11:44:06 +0900

    [add]ジョギングデータ画面を作成 close #1

--- resources/views/jog_reg.blade.php
+++ resources/views/jog_reg.blade.php
@@ -3,9 +3,28 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    
+    <title>Jogging-情報登録</title>
 </head>
 <body>
-    <p>これは登録画面</p>
+    <h1>新規データ登録</h1>
+    <main id="jog_reg">
+        <form action="#" method="get">
+            日付 <input type="date" name="date" required><br>
+            場所
+            <select name="blood_type" required>
+                <option value="o-place">屋外</option><br>
+                <option value="i-place">屋内</option><br>
+            </select><br>
+            距離 <input type="text" name="distance" placeholder="例:12.34" required>km <br>
+            時間 <input type="time" name="time"  step="1"><br>
+            消費カロリー <input type="text" name="kcal" placeholder="例:1234" required>kcal <br>
+            画像 <input type="file" id="fileElem" multiple accept="image/*" style="display:none" />
+                 <button id="fileSelect" type="button">ファイルを選択</button> <br>
+
+            <button class="cancel" type="reset">キャンセル</button>
+            <button class="btn-primary" type="submit">登録</button>
+        </form>
+    </main>
 </body>
 </html>
