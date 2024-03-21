823034b909cf65a4e0f1da16b35ba666042845bf
kikuchihibiki <113155569+kikuchihibiki@users.noreply.github.com>
2024-03-21 11:04:14 +0900

    [update]レイアウトファイルの再作成

---
+++ resources/views/layouts/jogging.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>@yield('title')</title>
+</head>
+
+<body class="joggingapp">
+    <header class="header">
+        <!-- ここにヘッダー -->
+    </header>
+    <div class="content">
+        @yield('content')
+    </div>
+</body>
+
+</html>
