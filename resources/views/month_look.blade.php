af80a6c78a6376e6403f19511d338c0b13a05b40
kkudou <k.kudou.sys23@morijyobi.ac.jp>
2024-03-21 10:38:38 +0900

    [add]月別一覧画面を作成

--- resources/views/month_look.blade.php
+++ resources/views/month_look.blade.php
@@ -1,11 +1,57 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <p>これは月別画面</p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('month_look','月別一覧画面')
+
+@section('content')
+    @section('header')
+    @endsection
+    <main id="month_look">
+        <div> <!-- カレンダー形式 -->
+            <div>
+                <a>[前月に移動するアイコン]</a>
+                <h1>[年]<span>年</span>[月]<span>月</span></h1> <!-- クリックすると<input type="month">が表示されるようにする(js使用) -->
+                <a>[翌月に移動するアイコン]</a>
+            </div>
+            <div>
+                [ここにトグルボタン]
+            </div>
+            [ここにカレンダー]
+        </div>
+
+        <div> <!-- リスト形式 -->
+            <div>
+                <a>[前月に移動するアイコン]</a>
+                <h1>[年]<span>年</span>[月]<span>月</span></h1> <!-- クリックすると<input type="month">が表示されるようにする(js使用) -->
+                <a>[翌月に移動するアイコン]</a>
+            </div>
+            <div>
+                <button type="button">[絞り込みアイコン]絞り込み</button>
+                [ここにトグルボタン]
+            </div>
+
+            <table>
+                <thead>
+                    <tr>
+                        <th>日付[並替アイコン]</th>
+                        <th>場所[並替アイコン]</th>
+                        <th>距離[並替アイコン]</th>
+                        <th>時間[並替アイコン]</th>
+                        <th>消費カロリー[並替アイコン]</th>
+                        <th></th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <tr>
+                        <td>[日付]</td>
+                        <td>[場所アイコン]</td>
+                        <td>[距離]<span>km</span></td>
+                        <td>[時間]</td>
+                        <td>[消費カロリー]<span>kcal</span></td>
+                        <td>[編集アイコン][削除アイコン]</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </main>
+@endsection
