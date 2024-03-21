d096b72a8f6890bd93116aae330291c63486f5d4
F.RN1945 <r.furukawa.sys23@morijyobi.ac.jp>
2024-03-21 09:55:18 +0900

    [update]ジョギングデータ登録画面を修正 close #1

--- resources/views/jog_reg.blade.php
+++ resources/views/jog_reg.blade.php
@@ -20,10 +20,10 @@
             </select><br>
 
             <span class="required">距離*</span> 
-            <input type="text" name="distance" pattern="\d*" placeholder="例:12.34" required>km <br>
+            <input type="number" step="0.1" name="distance" placeholder="例:12.34" required>km <br>
 
             時間 <input type="time" name="time" step="1"><br>
-            消費カロリー <input type="text" name="kcal" pattern="\d*" placeholder="例:1234">kcal <br>
+            消費カロリー <input type="number" name="kcal" placeholder="例:1234">kcal <br>
             画像 <input type="file" name="files"><br>
 
             <button class="cancel" type="reset">キャンセル</button>
