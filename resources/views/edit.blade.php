876c226ffb1e15a44abc0d234255857536aa49dc
F.RN1945 <r.furukawa.sys23@morijyobi.ac.jp>
2024-03-26 11:01:55 +0900

    [update] 登録と編集とcssを修正

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,24 +1,23 @@
 @extends('layouts.jogging')
 
 @section('title','ページのタイトル')
-<link rel="stylesheet" href="css/jog_form.css">
 
 @section('content')
     @section('header')
     @endsection
 
 
-<h1>データ編集</h1>
 <main id="edit">
+<h1>データ編集</h1>
     <form method="POST" action="{{ route('edit_function') }}">
         @csrf
         <table>
                 <tr>
-                    <th> 日付 <span>*</span></th>
+                    <th>日付<span class="required">*</span></th>
                     <td><input type="date" name="date" required><br></td>
                 </tr>
                 <tr>
-                    <th> 場所 </th>
+                    <th>場所</th>
                     <td>
                         <select name="place">
                             <option value="o-place">屋外</option><br>
@@ -27,19 +26,19 @@
                     </td>
                 </tr>
                 <tr>
-                    <th> 距離 <span>*</span></th>
-                    <td><input type="number" step="0.1" name="distance" placeholder="例:12.34" required>km<br></td>
+                    <th>距離<span class="required">*</span></th>
+                    <td><input type="number" step="0.01" name="distance" placeholder="例:12.34" required>km<br></td>
                 </tr>
                 <tr>
-                    <th> 時間  </th>
-                    <td><input type="time" name="time"  step="1"><br></td>
+                    <th>時間</th>
+                    <td><input type="time" name="time" step="1" value="00:00:00"><br></td>
                 </tr>
                 <tr>
-                    <th> 消費カロリー </th>
-                    <td><input type="number" name="kcal" placeholder="例:1234">kcal <br></td>
+                    <th>消費カロリー</th>
+                    <td><input type="number" name="kcal" placeholder="例:1234">kcal<br></td>
                 </tr> 
                 <tr>
-                    <th> 画像  </th>
+                    <th>画像</th>
                     <td><input type="file" name="files">
                     <img src="{{ asset($jogging->course_img_pass) }}" alt="コース画像">
                     </td>
@@ -49,8 +48,8 @@
 
 
             <input type="hidden" name="id" value="{{ $jogging->ID }}">
-            <button class="cancel" type="reset">キャンセル</button>
-            <button class="btn-primary" type="submit">更新</button>
+            <button class="btn" type="reset">キャンセル</button>
+            <button class="btn btn-submit" type="submit">更新</button>
     </form>
 </main>
 
