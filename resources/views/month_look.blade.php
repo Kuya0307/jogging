5d2991fa9280187c49f279405ebb5637cd7261d8
kuya <k.oda.sys22@morijyobi.ac.jp>
2024-03-26 11:25:15 +0900

    月の遷移ボタンをカレンダーリストで独立させた

--- resources/views/month_look.blade.php
+++ resources/views/month_look.blade.php
@@ -21,9 +21,6 @@
             background-color: #f2f2f2;
         }
     </style>
-
-<button onclick="previous_month()">次へ</button>
-<button onclick="next_month()">次へ</button>
 <div id="calendar"></div>
 <style>
         table {
@@ -53,8 +50,8 @@
         
         const monthNames = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
         
-        let table = '<table><tr><th colspan="7">'  + year + '年' + ' ' + monthNames[month - 1] + '</th></tr>';
-        table += '<tr><th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th></tr><tr>';
+        let cal_table = '<a onclick=previous_month()>[前月に移動するアイコン]</a><h1>'  + year + '年' + ' ' + monthNames[month - 1] + '</h1><a onclick=next_month()>[次月に移動するアイコン]</a>';
+        cal_table += '<table><tr><th>日</th><th>月</th><th>火</th><th>水</th><th>木</th><th>金</th><th>土</th></tr><tr>';
         
         let day = 1;
         for (let i = 0; i < 42; i++) {
@@ -62,18 +59,18 @@
                 break;
             }
             if (i < firstDayIndex || day > daysInMonth) {
-                table += '<td></td>';
+                cal_table += '<td></td>';
             } else {
-                table += '<td><a href="/view?year=' + year + '&month=' + month + '&day=' + day + '">' + day + '</a></td>';
+                cal_table += '<td><a href="/view?year=' + year + '&month=' + month + '&day=' + day + '">' + day + '</a></td>';
                 day++;
             }
             if (i % 7 === 6) {
-                table += '</tr><tr>';
+                cal_table += '</tr><tr>';
             }
         }
         
-        table += '</tr></table>';
-        calendarDiv.innerHTML = table;
+        cal_table += '</tr></table>';
+        calendarDiv.innerHTML = cal_table;
     }
 
     // カレンダーの生成
@@ -101,41 +98,116 @@
     }
 </script>
 
+
+
+
+
+
+
+
+<!--ここからリスト-->
+
+
+
+
+
+
+
+
+
+
+
 <div> <!-- リスト形式 -->
     <div>
-        <a>[前月に移動するアイコン]</a>
-        <h1>[年]<span>年</span>[月]<span>月</span></h1> <!-- クリックすると<input type="month">が表示されるようにする(js使用) -->
-        <a>[翌月に移動するアイコン]</a>
+        <h3 id="ListHeader"></h3>
     </div>
     <div>
         <button type="button">[絞り込みアイコン]絞り込み</button>
-        [ここにトグルボタン]
+        <!-- ここにトグルボタン -->
     </div>
 
     <table>
         <thead>
             <tr>
-                <th>日付[並替アイコン]</th>
-                <th>場所[並替アイコン]</th>
-                <th>距離[並替アイコン]</th>
-                <th>時間[並替アイコン]</th>
-                <th>消費カロリー[並替アイコン]</th>
+                <th><a href="#">日付</a>[並替アイコン]</th>
+                <th><a href="#">場所</a>[並替アイコン]</th>
+                <th><a href="#">距離</a>[並替アイコン]</th>
+                <th><a href="#">時間</a>[並替アイコン]</th>
+                <th><a href="#">消費カロリー</a>[並替アイコン]</th>
                 <th></th>
             </tr>
         </thead>
 
-        <tbody>
-            <tr>
-                <td>[日付]</td>
-                <td>[場所アイコン]</td>
-                <td>[距離]<span>km</span></td>
-                <td>[時間]</td>
-                <td>[消費カロリー]<span>kcal</span></td>
-                <td>[編集アイコン][削除アイコン]</td>
-            </tr>
+        <tbody id="calendarBody">
+            <!-- カレンダーデータがここに挿入される -->
         </tbody>
     </table>
 </div>
+
+<script>
+    let currentYear_list = {{ $year }};
+    let currentMonth_list = {{ $month }};
+
+    function generateList(year, month) {
+        const calendarHeader = document.getElementById('ListHeader');
+        const calendarBody = document.getElementById('calendarBody');
+        const date = new Date(year, month - 1, 1);
+        const daysInMonth = new Date(year, month, 0).getDate();
+        const firstDayIndex = date.getDay();
+
+        const monthNames = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+
+        ListHeader.innerHTML = '<a onclick="previous_month_list()">[前月に移動するアイコン]</a>' +
+                                    '<h1>' + year + '年' + ' ' + monthNames[month - 1] + '</h1>' +
+                                    '<a onclick="next_month_list()">[翌月に移動するアイコン]</a>';
+
+        let list_table = '';
+        let day = 1;
+        for (let i = 0; i < 42; i++) {
+            if (i === firstDayIndex + daysInMonth) {
+                break;
+            }
+            if (i < firstDayIndex || day > daysInMonth) {
+                list_table += '<tr><td></td></tr>';
+            } else {
+                list_table += '<tr><td><a href="/view?year=' + year + '&month=' + month + '&day=' + day + '">' + day + '</a></td>' +
+                         '<td>[場所アイコン]</td>' +
+                         '<td>[距離]<span>km</span></td>' +
+                         '<td>[時間]</td>' +
+                         '<td>[消費カロリー]<span>kcal</span></td>' +
+                         '<td>[編集アイコン][削除アイコン]</td></tr>';
+                day++;
+            }
+        }
+
+        calendarBody.innerHTML = list_table;
+    }
+
+    // 初期表示
+    generateList(currentYear_list, currentMonth_list);
+
+    // 前月へ
+    function previous_month_list() {
+        if (currentMonth === 1) {
+            currentYear--;
+            currentMonth = 12;
+        } else {
+            currentMonth--;
+        }
+        generateList(currentYear, currentMonth);
+    }
+
+    function next_month_list() {
+        if (currentMonth === 12) {
+            currentYear++;
+            currentMonth = 1;
+        } else {
+            currentMonth++;
+        }
+        generateList(currentYear, currentMonth);
+    }
+</script>
+
 
 
 </main>
