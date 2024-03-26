a7bed7091227a684eab5bcb9b6a546a41ed85750
F.RN1945 <r.furukawa.sys23@morijyobi.ac.jp>
2024-03-26 10:19:22 +0900

    [update] 編集画面の修正

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -7,30 +7,6 @@
     @section('header')
     @endsection
 
-    <h1>データ編集</h1>
-    <main id="edit">
-        <form action="#" method="get">
-            <span class="required">日付</span>
-            <input type="date" name="date" required><br>
-
-            場所
-            <select name="blood_type">
-                <option value="o-place">屋外</option><br>
-                <option value="i-place">屋内</option><br>
-            </select><br>
-
-            <span class="required">距離</span>
-            <input type="number" step="0.1" name="distance" placeholder="例:12.34" required>km <br>
-
-            時間 <input type="time" name="time" step="1"><br>
-            消費カロリー <input type="number" name="kcal" placeholder="例:1234">kcal <br>
-            画像 <input type="file" name="files"><br>
-
-            <button class="cancel" type="reset">キャンセル</button>
-            <button class="btn-primary" type="submit">更新</button>
-        </form>
-    </main>
-@endsection
 
 <h1>データ編集</h1>
 <main id="edit">
@@ -70,8 +46,6 @@
 
                 </tr>
             </table>
-      
-
 
 
             <input type="hidden" name="id" value="{{ $jogging->ID }}">
